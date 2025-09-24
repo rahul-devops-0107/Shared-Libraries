ddb2752a398ace6e4177dbadfb286b0c33f8ca16
rahul-devops-0107 <rahul.devops.0107@gmail.com>
2025-09-24 10:29:47 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -1,24 +1,24 @@
-def git-checkout(repo)
+def GitCheckout(repo)
 {
     git "https://github.com/IntelliqDevops/${repo}.git" // We are replacing the repo name so as to reuse it in multiple pipelines
 }
 
-def build-artifact()
+def BuildArtifact()
 {
     sh 'mv clean package' // We are not using any variables here because the command is common in every pipeline and "clean package" is used so as to reduce the memory consumption of jenkins server as it deletes previous build artifacts once the new build stage is trigerred
 }
 
-def tomcat-deployment(jobname,ip_address,context) // Here we are using three variables to reuse in different deployment stages as three variable change during the deployment stages
+def TomcatDeployment(jobname,ip_address,context) // Here we are using three variables to reuse in different deployment stages as three variable change during the deployment stages
 {
     sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war vagrant@${ip_address}:var/lib/tomcat10/webapps/${context}.war"
 }
 
-def testing-run(jobname)
+def TestingRun(jobname)
 {
     sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
 }
 
-def email-notification(stage_error,subject_body,email)
+def EmailNotification(stage_error,subject_body,email)
 {
     mail bcc: "", body: "${stage_error}.getMessage()", cc: "", from: "", replyTo: "", subject: "${subject_body}", to: "${email}"
     exit(1)
